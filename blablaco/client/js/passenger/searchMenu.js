15435ecb73aabe643fb57bde83e5d044ed02dedd
Lucas OH <luky.herbin@free.fr>
2018-01-04 10:00:02 +0100

    [LOH] Switch depart-arrivee

--- blablaco/client/js/passenger/searchMenu.js
+++ blablaco/client/js/passenger/searchMenu.js
@@ -1,5 +1,7 @@
 Template.searchMenu.events({
-  'keyup .text-input-trigger' : hideShowSideOptions
+  'keyup .text-input-trigger' : hideShowSideOptions,
+  'click .voice-button' : vocalInput,
+  'click .switch-button' :  switchInputs
 });
 
 function hideShowSideOptions(event, template) {
@@ -8,4 +10,14 @@ function hideShowSideOptions(event, template) {
   }else {
     $('.search-advanced-options').attr('hidden', true);
   }
+}
+
+function vocalInput(event, template){
+  alert('Enregistrement audio...');
+}
+
+function switchInputs(event, template){
+  var temp = $('#depart-input').val();
+  $('#depart-input').val($('#arrivee-input').val());
+  $('#arrivee-input').val(temp);
 }
