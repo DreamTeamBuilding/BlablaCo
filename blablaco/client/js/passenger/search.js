d7c100dcd99d639235ac3a1654f08af1e229216c
Lucas OH <luky.herbin@free.fr>
2018-01-04 14:48:53 +0100

    [LOH] Poufff, le résultat de recherches donne enfin qqch !

--- blablaco/client/js/passenger/search.js
+++ blablaco/client/js/passenger/search.js
@@ -4,6 +4,12 @@ Template.searchMenu.events({
   'click .switch-button' :  switchInputs
 });
 
+Template.searchResults.events({
+  'click .switch-button' :  switchInputs,
+  'click .hide-filters' :  hideFilters,
+  'click .show-filters' :  showFilters
+});
+Template.searchResults.onRendered(initFilters);
 function hideShowSideOptions(event, template) {
   if($('#depart-input').val()!="" && $('#arrivee-input').val()!=""){
     $('.search-advanced-options').removeAttr('hidden');
@@ -20,4 +26,18 @@ function switchInputs(event, template){
   var temp = $('#depart-input').val();
   $('#depart-input').val($('#arrivee-input').val());
   $('#arrivee-input').val(temp);
+}
+
+function hideFilters(event, template){
+    $('.replace-filters').removeAttr('hidden');
+    $('.filters').attr('hidden', true);
+}
+
+function showFilters(event, template){
+    $('.filters').removeAttr('hidden');
+    $('.replace-filters').attr('hidden', true);
+}
+
+function initFilters(){
+  hideFilters(null, null);
 }
