af9b0a72df85652b07073aea3953d02b06319f62
Lucas OH <luky.herbin@free.fr>
2018-01-09 20:03:38 +0100

    [LOH] Mise en page formulaire proposer trajet

--- blablaco/client/js/driver/formAddTravel.js
+++ blablaco/client/js/driver/formAddTravel.js
@@ -1,5 +1,9 @@
 Template.formAddTravel.events({
-  'change .toggle-return' : hideReturnMenu
+  'change .toggle-return' : hideReturnMenu,
+  'click #less-money' : decreaseCost,
+  'click #more-money' : increaseCost,
+  'click #less-passengers' : decreasePassengerCount,
+  'click #more-passengers' : increasePassengerCount
 });
 
 function hideReturnMenu(event, template){
@@ -8,4 +12,30 @@ function hideReturnMenu(event, template){
   } else {
     $('.return-form').removeAttr('hidden');
   }
+}
+function decreaseCost(event, template){
+  event.preventDefault();
+  value = Math.max(0,parseInt($("#input-price").val()) - 1);
+  $("#input-price").val(value.toString());
+}
+
+function increaseCost(event, template){
+  event.preventDefault();
+  value = parseInt($("#input-price").val()) + 1;
+  $("#input-price").val(value.toString());
+}
+function decreasePassengerCount(event, template){
+  event.preventDefault();
+  value = Math.max(1,parseInt($("#input-number-passengers").val()) - 1);
+  $("#input-number-passengers").val(value.toString());
+  if(value==1){
+    $("#label-number-passengers").html('passager');
+  }
+}
+
+function increasePassengerCount(event, template){
+  event.preventDefault();
+  value = parseInt($("#input-number-passengers").val()) + 1;
+  $("#input-number-passengers").val(value.toString());
+  $("#label-number-passengers").html('passagers');
 }
