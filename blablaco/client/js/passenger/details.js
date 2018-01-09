d58fef6ec04cd049784034e1c0d8de513c0c4af5
cpourcel <clara.pourcel@insa-lyon.fr>
2018-01-10 00:18:31 +0100

    [CP] notification téléphone

--- blablaco/client/js/passenger/details.js
+++ blablaco/client/js/passenger/details.js
@@ -4,4 +4,9 @@ Template.details.events({
 
 function showNotif(event, template){
   $('#notification-reservation').removeAttr('hidden');
+  window.setTimeout(showPhoneNotification, 1000);
+}
+
+function showPhoneNotification() {
+  $('#dialog').dialog({ width: $(window).width() /* insert your options */ }).dialog('widget').position({ my: 'top', at: 'top', of: $(this) });
 }
